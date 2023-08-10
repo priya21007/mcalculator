47aad3e0081271e5795aa365b90d1bd7762c1040
PL LAB <PL LAB@PLLAB71>
2023-08-10 05:42:02 +0530

    first pro

---
+++ src/mcalculator/addition.java
@@ -0,0 +1,5 @@
+package mcalculator;
+
+public class addition {
+
+}
---
+++ src/mcalculator/mcalculator.java
@@ -0,0 +1,10 @@
+package mcalculator;
+
+public class mcalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
